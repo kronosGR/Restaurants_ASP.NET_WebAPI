73d0a5b99de07512c29f49df75914a58eff8a1ff
kronosGR <geo.elgeo@gmail.com>
2025-08-22 11:39:18 +0200

    added entities and dbcontenxt

---
+++ Restaurants/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+public class RestaurantsDbContext:DbContext
+{
+    
+    public RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> options):base(options){}
+    
+    internal DbSet<Restaurant> Restaurants {get; set;}
+    internal DbSet<Dish> Dishes {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "Host=kandz.me;Port=3306;Database=restaurants;Username=restaurants;Password=KnAXxnHGx4G3FhKc;",
+            new MariaDbServerVersion(new Version(8,0,32)));
+    }
+}
