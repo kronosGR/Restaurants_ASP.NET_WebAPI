649b72763d9ac7d0a14d398088549ba99ab9b1b8
kronosGR <geo.elgeo@gmail.com>
2025-08-22 11:51:35 +0200

    small changes and first migration

--- Restaurants/Persistence/RestaurantsDbContext.cs
+++ Restaurants/Persistence/RestaurantsDbContext.cs
@@ -13,8 +13,19 @@ public class RestaurantsDbContext:DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySql(
-            "Host=kandz.me;Port=3306;Database=restaurants;Username=restaurants;Password=KnAXxnHGx4G3FhKc;",
-            new MariaDbServerVersion(new Version(8,0,32)));
+        // optionsBuilder.UseMySql(
+        //     "Host=kandz.me;Port=3306;Database=restaurants;Username=restaurants;Password=KnAXxnHGx4G3FhKc;",
+        //     new MariaDbServerVersion(new Version(8,0,32)));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
     }
 }
--- Restaurants/Program.cs
+++ Restaurants/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<RestaurantsDbContext>(options =>
+    options.UseMySql(
+        "Host=kandz.me;Port=3306;Database=restaurants;Username=restaurants;Password=KnAXxnHGx4G3FhKc;",
+        new MariaDbServerVersion(new Version(8,0,32))));
 
 var app = builder.Build();
 
